c4dc8d304382378dfc8e086fad950241038375fa
arunkumar2345 <98070875+arunkumar2345@users.noreply.github.com>
2022-02-16 13:38:26 +0530

    Add files via upload

---
+++ Prac1.java
@@ -0,0 +1,24 @@
+package Testofmavenproject.google;
+
+public class Prac1 {
+	
+	private double val;
+
+	public double getVal() {
+		return val;
+	}
+
+	public void setVal(double val) {
+		this.val = val;
+	}
+
+	
+	}
+	
+	
+	
+
+	
+	
+
+
